74de22c141071b100203dbece04885d39b03ee0f
Guy Royse <guy@guyroyse.com>
2020-05-01 20:29:23 -0400

    api to return title and subtitle

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const Redis = require("ioredis")
+
 const express = require('express')
 const app = express()
 const port = 3000
 
+const redis = new Redis('redis://powerspork.local:6379')
+
 app.use(express.static('static'))
+
+app.get('/api/stream-info', async (req, res) => {
+
+  let title = await redis.get('stream:assets:title')
+  let subtitle = await redis.get('stream:assets:subtitle')
+  res.send({ title, subtitle })
+
+})
 
 app.listen(port)
